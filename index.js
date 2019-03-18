2a72d4ad36f8a00dd70bfc8bce0dcf57889a3650
JoshKeyz <joshua.oguma@outlook.com>
2019-03-18 07:55:08 +0200

    Added options method handling for preflights requests

--- index.js
+++ index.js
@@ -64,6 +64,8 @@ const server = (req, res)=>{
       // Send response to the client
       res.statusCode = statusCode;
       res.setHeader('Content-Type', 'application/json')
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
       res.end(payloadStr);
 
       // returning the response
